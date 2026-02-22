5560c2206e239a032326c68b87e3ba6a4f0d4b75
AfshinD-W <afshinaa705@gmail.com>
2026-02-22 22:39:41 +0330

    Include GRPC.ASP to server and create proto file

--- WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66f5f45b1df7954c7838c258663455ce889aa5f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd2c451ba075ce16ae557087616495e8b7123da3")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
